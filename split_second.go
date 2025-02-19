c8f935bf25c40bd4f48400317e0a4260b4ff9f2d
sheetal gupta <150527877+GuptaGeek@users.noreply.github.com>
2025-02-19 13:15:14 +0530

    Create split_second.go

---
+++ split_second.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/inancgumus/screen"
+)
+
+type placeholder [5]string
+
+func main() {
+
+	for j := 0; j < 10; j++ {
+		screen.Clear()
+		screen.MoveTopLeft()
+		RetroLedClock()
+		time.Sleep(time.Second)
+	}
+
+}
+func RetroLedClock() {
+
+	digits := [10]placeholder{
+
+		[5]string{"███", "█ █", "█ █", "█ █", "███"},
+		[5]string{"  █", "  █", "  █", "  █", "  █"},
+		[5]string{"███", "  █", "███", "█  ", "███"},
+		[5]string{"███", "  █", "███", "  █", "███"},
+		[5]string{"█ █", "█ █", "███", "  █", "  █"},
+		[5]string{"███", "█  ", "███", "  █", "███"},
+		[5]string{"███", "█  ", "███", "█ █", "███"},
+		[5]string{"███", "  █", "  █", "  █", "  █"},
+		[5]string{"███", "█ █", "███", "█ █", "███"},
+		[5]string{"███", "█ █", "███", "  █", "███"},
+	}
+	alarm := [8]placeholder{
+		[5]string{" ", " ", " ", " ", " "},
+		[5]string{"███", "█ █", "███", "█ █", "█ █"},
+		[5]string{"█  ", "█  ", "█  ", "█  ", "███"},
+		[5]string{"███", "█ █", "███", "█ █", "█ █"},
+		[5]string{"███", "█ █", "██ ", "█ █", "█ █"},
+		[5]string{"█ █", "███", "█ █", "█ █", "█ █"},
+		[5]string{"█  ", "█  ", "█  ", " ", "█  "},
+		[5]string{" ", " ", " ", " ", " "},
+	}
+	seperator := placeholder{"   ", " █ ", "   ", " █ ", "   "}
+	spaces := placeholder{"   ", "   ", "   ", "   ", "   "}
+	dot := placeholder{"   ", "   ", "   ", "   ", " █ "}
+	now := time.Now()
+	hour, minute, second := now.Hour(), now.Minute(), now.Second()
+	ssec := now.Nanosecond() / 100000000
+	clock := [...]int{hour / 10, hour % 10, 10, minute / 10, minute % 10, 10, second / 10, second % 10, 11, ssec}
+	if second%10 == 0 {
+		for i := 0; i < 5; i++ {
+			for j := 0; j < 8; j++ {
+				fmt.Print(alarm[j][i], " ")
+			}
+			fmt.Println()
+		}
+	} else {
+
+		for i := 0; i <= 4; i++ {
+			for _, digit := range clock {
+				if digit == 10 && second%2 != 0 {
+					fmt.Print(seperator[i])
+				} else if digit == 10 && second%2 == 0 {
+					fmt.Print(spaces[i])
+				} else if digit == 11 && second%2 != 0 {
+					fmt.Print(dot[i])
+				} else if digit == 11 && second%2 == 0 {
+					fmt.Print(spaces[i])
+				} else {
+					fmt.Print(digits[digit][i], " ")
+				}
+			}
+			fmt.Println()
+		}
+	}
+
+}
